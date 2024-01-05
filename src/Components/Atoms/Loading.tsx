b9cee2417e54fb44b1a0419a9af87adc1e6dd666
Jose Andres <Gorusuke123@gmail.com>
2024-01-05 17:18:56 -0500

    Add wouter for routes and creating a loadingPage

---
+++ src/Components/Atoms/Loading.tsx
@@ -0,0 +1,14 @@
+import '../../App.css'
+
+const Loading = () => {
+  return (
+    <div className='loading-container'>
+      <img
+        src="../../../public/BrawlStars.svg"
+        alt="Loading"
+      />
+    </div>
+  )
+}
+
+export default Loading
