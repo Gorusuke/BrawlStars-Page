b802b50a013cb6c1bf9a3a0917bf6a6d508323b5
Jose Andres <Gorusuke123@gmail.com>
2024-01-05 17:21:38 -0500

    Add some styles and new components

--- src/Components/Atoms/Powers.tsx
+++ src/Components/Atoms/Powers.tsx
@@ -2,17 +2,34 @@ import { StarPower } from "../../interfaces/brawler"
 
 interface Props {
   power: StarPower;
-  className: string
+  className?: string;
+  tooltip?: boolean;
 }
 
-const Powers = ({power, className}: Props) => {
+const Powers = ({ power, className = 'powers-image', tooltip }: Props) => {
   return (
-    <img
-      className={className}
-      key={power.id}
-      src={power.imageUrl}
-      alt={power.name}
-    />
+    <>
+      {!tooltip &&
+        <img
+          className={className}
+          src={power.imageUrl}
+          alt={power.name}
+        />
+      }
+      {tooltip &&
+        <div className="tooltip">
+          <div className="tooltip-content">
+            <span><b>{power.name}</b></span>
+            <span>{power.description}</span>
+          </div>
+          <img
+            className={className}
+            src={power.imageUrl}
+            alt={power.name}
+          />
+        </div>
+      }
+    </>
   )
 }
 
--- src/Components/Brawler/Brawler.tsx
+++ src/Components/Brawler/Brawler.tsx
@@ -1,27 +1,49 @@
+import { useEffect, useState } from "react"
+import { useParams } from "wouter"
 import { BrawlerInterface } from "../../interfaces/brawler"
+import Loading from "../Atoms/Loading"
 import Powers from "../Atoms/Powers"
-import './styles.css' 
+import './styles.css'
 
-function Brawler({brawler}: {brawler: BrawlerInterface}) {
+const Brawler = () => {
+  const [brawler, setBrawler] = useState({} as BrawlerInterface)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const { id } = useParams()
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(`https://api.brawlapi.com/v1/brawlers/${id}`)
+      .then(res => res.json())
+      .then(res => setBrawler(res))
+      .finally(() => setIsLoading(false))
+  }, [id])
+
   return (
-    <div className="brawler">
-      <div className="container">
-        <img
-          className="image"
-          src={brawler.imageUrl2}
-          alt={brawler.name}
-        />
-        <div className="info">
-          <h3>{brawler.name}</h3>
-          <span>{brawler.class.name}</span>
-          <span>{brawler.rarity.name}</span>
-          <div className="powers">
-            {brawler.starPowers.map(power => <Powers key={power.id} power={power} className="powers-image" />)}
-            {brawler.gadgets.map(gadget => <Powers key={gadget.id} power={gadget} className="powers-image" />)}
+    <>
+      {isLoading && <Loading />}
+      {!isLoading && Boolean(Object.values(brawler).length) &&
+        <div className="brawler-container">
+          <div className="container">
+            <section className="info-container">
+              <img
+                className="image"
+                src={brawler.imageUrl3}
+                alt={brawler.name}
+              />
+              <div className="info">
+                <h2>{brawler.name}</h2>
+                <span>{brawler.description}</span>
+                <span>{brawler.class.name}</span>
+                <span>{brawler.rarity.name}</span>
+                <div className="powers">
+                  {[...brawler.starPowers, ...brawler.gadgets].map(gadget => <Powers key={gadget.id} power={gadget} tooltip />)}
+                </div>
+              </div>
+            </section>
           </div>
         </div>
-      </div>
-    </div>
+      }
+    </>
   )
 }
 
