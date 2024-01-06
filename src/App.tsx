8f029dfe5bdfba645f92bcecf4b54c25d3568ca2
Jose Andres <Gorusuke123@gmail.com>
2024-01-06 16:33:07 -0500

    Add search and sort filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
+import { useState } from 'react'
 import { Route, Switch } from 'wouter'
 import Brawlers from './Components/Brawlers/Brawlers'
 import Brawler from './Components/Brawler/Brawler'
+import NavArrow from '/NavArrow.svg'
+import Search from '/Search.svg'
 import './App.css'
 
-function App() {  
+function App() {
+  const [showfilters, setShowFilters] = useState<boolean>(false);
   return (
-    <Switch>
-      <Route path='/' component={Brawlers} />
-      <Route path='/brawler/:id' component={Brawler} />
-    </Switch>
+    <>
+      <h1>sdfhsjkdfhsjkfskjfhdf</h1>
+      <section className='filter-container'>
+        <div className='search'>
+          <input type="text" placeholder='Type a brawler name' />
+          <img src={Search} alt="Search" />
+        </div>
+        <div className='sort'>
+          <button
+            className='sort-button'
+            onClick={() => setShowFilters((prev) => !prev)}
+          >
+            Sort <img src={NavArrow} alt="Arrow" />
+          </button>
+          {showfilters && 
+            <ul>
+              <li>Rarity</li>
+              <li>Class</li>
+            </ul>
+          }
+        </div>
+      </section>
+      <div className='root'>
+        <Switch>
+          <Route path='/' component={Brawlers} />
+          <Route path='/brawler/:id' component={Brawler} />
+        </Switch>
+      </div>
+    </>
   )
 }
 
