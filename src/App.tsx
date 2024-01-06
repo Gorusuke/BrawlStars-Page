5153cd87d5511636c57ef27631b096c3a3b55849
Jose Andres <Gorusuke123@gmail.com>
2024-01-05 19:45:48 -0500

    Add switch to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Route } from 'wouter'
+import { Route, Switch } from 'wouter'
 import Brawlers from './Components/Brawlers/Brawlers'
 import Brawler from './Components/Brawler/Brawler'
 import './App.css'
 
 function App() {  
   return (
-    <>
+    <Switch>
       <Route path='/' component={Brawlers} />
       <Route path='/brawler/:id' component={Brawler} />
-    </>
+    </Switch>
   )
 }
 
