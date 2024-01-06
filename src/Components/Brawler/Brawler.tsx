bcf4599d89d12e6858625717c944f3f5e18e1389
Jose Andres <Gorusuke123@gmail.com>
2024-01-05 23:05:16 -0500

    Add some styles and makeing some changes

--- src/Components/Brawler/Brawler.tsx
+++ src/Components/Brawler/Brawler.tsx
@@ -18,6 +18,14 @@ const Brawler = () => {
       .finally(() => setIsLoading(false))
   }, [id])
 
+  const cutText = (text: string) => {
+    if (text.length > 150) {
+      const splitText = text.split('.', 2)
+      return `${splitText.join('.')}.`
+    }
+    return text
+  }
+
   return (
     <>
       {isLoading && <Loading />}
@@ -27,14 +35,20 @@ const Brawler = () => {
             <section className="info-container">
               <img
                 className="image"
-                src={brawler.imageUrl3}
+                src={brawler.imageUrl2}
                 alt={brawler.name}
+                style={{border: `16px solid ${brawler.rarity.color}`}}
               />
+              {/* <div style={{ backgroundColor: brawler.rarity.color }} className="test">
+                <h2>{brawler.name}</h2>
+                <span className="class">{brawler.class.name}</span>
+                <span style={{color: brawler.rarity.color}}>{brawler.rarity.name}</span> 
+              </div> */}
               <div className="info">
                 <h2>{brawler.name}</h2>
-                <span>{brawler.description}</span>
+                <span className="description">{cutText(brawler.description)}</span>
                 <span className="class">{brawler.class.name}</span>
-                <span style={{color: brawler.rarity.color}}>{brawler.rarity.name}</span>
+                <span className='rarity' style={{color: brawler.rarity.color}}>{brawler.rarity.name}</span>
                 <div className="powers">
                   {[...brawler.starPowers, ...brawler.gadgets].map(gadget => <Powers key={gadget.id} power={gadget} tooltip />)}
                 </div>
