aa5d8379df9eb0a6ace22331bed38e0dba9873f1
Jose Andres <Gorusuke123@gmail.com>
2024-01-07 15:36:42 -0500

    Add tsignore

--- src/Components/BrawlersByType/BrawlersByType.tsx
+++ src/Components/BrawlersByType/BrawlersByType.tsx
@@ -13,7 +13,8 @@ const BrawlersByType = ({ brawlers, selection }: { brawlers: BrawlerInterface[],
         <section className="types-container" key={name}>
           <h2>{name}</h2>
           <div className="image-container">
-            {result[name as keyof AllTypes].map((brawler: BrawlerInterface) => (
+            { // @ts-expect-error Property map does not exist on type never.
+              result[name as keyof AllTypes].map((brawler: BrawlerInterface) => (
               <Link  key={brawler.id} href={`/brawler/${brawler.id}`}>
                 <img
                   className="images"
