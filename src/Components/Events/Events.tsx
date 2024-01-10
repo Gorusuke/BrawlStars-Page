41fb1ed53a274959260f6524f3b2a3ac753eb4b2
Jose Andres <Gorusuke123@gmail.com>
2024-01-10 16:59:54 -0500

    Refactor event page to show only active events

--- src/Components/Events/Events.tsx
+++ src/Components/Events/Events.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
-import { Active, Events, Upcoming } from '../../interfaces/events'
+import { Events } from '../../interfaces/events'
 import { getAllEvents } from '../../Utils'
 import Loading from '../Atoms/Loading'
 import './styles.css'
+
 const Events = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [brawlEvents, setBrawlEvents] = useState<Events>({} as Events)
@@ -25,50 +26,91 @@ const Events = () => {
   return (
     <>
       {isLoading && <Loading />}
-      {!isLoading && 
+      {!isLoading && Object.keys(brawlEvents).length &&
         <>
           <h1 className='title'>Events</h1>
-          <section>
-            {Object.entries(brawlEvents).map(brawlEvent => {
-              const [title, events] = brawlEvent
-              return (
-                <section key={title} className='event-container'>
-                  <h2>{title}</h2>
-                  <div className='event-card-container'>
-                    {events.map((event: Active | Upcoming) => {
-                      const now = new Date().toLocaleString("en-US")
-                      const eventDate = new Date(event.endTime).toLocaleString("en-US")
-                      const [diffHour, diffMinutes] = getDate(eventDate, now)
-                      const hour = Number(diffHour)
-                      const minutes = Math.floor(Number('0.' + diffMinutes) * 60)
-                      return (
-                        <div key={event.map.id} className='card-container' >
-                          <div className='event-card-container-info'>
-                            <span className='event-time'>New Event in: {`${hour}h ${minutes}m`}</span>
-                            <img className='event-card-background' src={event.map.environment.imageUrl} alt={event.map.environment.name} />
-                            <div className='event-card-info' style={{ backgroundColor: event.map.gameMode.bgColor }}>
-                              <img className='event-card-image' src={event.map.gameMode.imageUrl} alt={event.map.gameMode.name} />
-                              <div className='event-card-title'>
-                                <h3>{event.map.gameMode.name.toUpperCase()}</h3>
-                                <span>{event.map.name}</span>
-                              </div>
-                            </div>
-                          </div>
-                          <div>
-                            Siguiente Seccion
+          <section className='event-container'>
+            <h2>Active</h2>
+            <div className='event-card-container'>
+              {brawlEvents.active.map(brawlEvent => {
+                const now = new Date().toLocaleString("en-US")
+                const eventDate = new Date(brawlEvent.endTime).toLocaleString("en-US")
+                const [diffHour, diffMinutes] = getDate(eventDate, now)
+                const hour = Number(diffHour)
+                const minutes = Math.floor(Number('0.' + diffMinutes) * 60)
+                return (
+                    <div key={brawlEvent.map.id} className='card-container'>
+                      <div className='event-card-container-info'>
+                        <span className='event-time'>New Event in: {`${hour}h ${minutes}m`}</span>
+                        <img className='event-card-background' src={brawlEvent.map.environment.imageUrl} alt={brawlEvent.map.environment.name} />
+                        <div className='event-card-info' style={{ backgroundColor: brawlEvent.map.gameMode.bgColor }}>
+                          <img className='event-card-image' src={brawlEvent.map.gameMode.imageUrl} alt={brawlEvent.map.gameMode.name} />
+                          <div className='event-card-title'>
+                            <h3>{brawlEvent.map.gameMode.name.toUpperCase()}</h3>
+                            <span>{brawlEvent.map.name}</span>
                           </div>
                         </div>
-                      )
-                    })}
-                  </div>
-                </section>
-              )
-            })}
+                      </div>
+                      <div>
+                        Siguiente Seccion
+                      </div>
+                    </div>
+                )
+              })}
+            </div>
           </section>
         </>
       }
     </>
   )
+
+//   return (
+//     <>
+//       {isLoading && <Loading />}
+//       {!isLoading && 
+//         <>
+//           <h1 className='title'>Events</h1>
+//           <section>
+//             {Object.entries(brawlEvents).map(brawlEvent => {
+//               const [title, events] = brawlEvent
+//               return (
+//                 <section key={title} className='event-container'>
+//                   <h2>{title}</h2>
+//                   <div className='event-card-container'>
+//                     {events.map((event: Active | Upcoming) => {
+//                       const now = new Date().toLocaleString("en-US")
+//                       const eventDate = new Date(event.endTime).toLocaleString("en-US")
+//                       const [diffHour, diffMinutes] = getDate(eventDate, now)
+//                       const hour = Number(diffHour)
+//                       const minutes = Math.floor(Number('0.' + diffMinutes) * 60)
+//                       return (
+//                         <div key={event.map.id} className='card-container' >
+//                           <div className='event-card-container-info'>
+//                             <span className='event-time'>New Event in: {`${hour}h ${minutes}m`}</span>
+//                             <img className='event-card-background' src={event.map.environment.imageUrl} alt={event.map.environment.name} />
+//                             <div className='event-card-info' style={{ backgroundColor: event.map.gameMode.bgColor }}>
+//                               <img className='event-card-image' src={event.map.gameMode.imageUrl} alt={event.map.gameMode.name} />
+//                               <div className='event-card-title'>
+//                                 <h3>{event.map.gameMode.name.toUpperCase()}</h3>
+//                                 <span>{event.map.name}</span>
+//                               </div>
+//                             </div>
+//                           </div>
+//                           <div>
+//                             Siguiente Seccion
+//                           </div>
+//                         </div>
+//                       )
+//                     })}
+//                   </div>
+//                 </section>
+//               )
+//             })}
+//           </section>
+//         </>
+//       }
+//     </>
+//   )
 }
 
 export default Events
