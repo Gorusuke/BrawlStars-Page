d9cc1dd73d4acdc5b37ad7789b51da823977b2c3
Jose Andres <Gorusuke123@gmail.com>
2024-01-12 19:44:39 -0500

    Change footer design and styles

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -19,28 +19,29 @@ const Layout = ({children}: {children: ReactNode}) => {
       {children}
       <footer>
         <nav>
+          <BrawlStarsLogo />
           <ul>
             <Link href='/brawlers'><a>Brawlers</a></Link>
             <Link href='/maps'><a>Maps</a></Link>
             <Link href='/events'><a>Events</a></Link>
             <Link href='/game-modes'><a>Game Modes</a></Link>
           </ul>
-          <BrawlStarsLogo />
         </nav>
         <div>
-          <p>
-            This content is not affiliated with, endorsed, sponsored, or specifically approved by Supercell and Supercell is not responsible for this content.<br />
-            For more information, see the <a href="https://supercell.com/en/fan-content-policy/" target="_blank">Fan Content Policy at Supercell Fan Content Policy.</a>
-          </p>
           <p className="">
             Design inspired by these pages
             <a href="https://brawlify.com/" target="_blank">Brawlify</a>
+            and
             <a href="https://brawlstats.com/" target="_blank">Brawlstats</a>
           </p>
           <p>
             Credit about Brawl Stars API to<a href="https://brawlify.com/" target="_blank">Brawlify</a>
           </p>
           <p>See documentation<a href="https://brawlapi.com/" target="_blank">here</a></p>
+          <p className="policy">
+            This content is not affiliated with, endorsed, sponsored, or specifically approved by Supercell and Supercell is not responsible for this content.
+            For more information, see the<a href="https://supercell.com/en/fan-content-policy/" target="_blank">Fan Content Policy at Supercell Fan Content Policy.</a>
+          </p>
         </div>
       </footer>
     </div>
